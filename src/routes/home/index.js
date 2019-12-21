5bad90898ccf6fb2727fa28fa7e2d248f9f70162
MagicMarvMan <marvin.kai@gmx.de>
2019-12-21 12:59:57 +0100

    Add basic home page and fetch statement

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,11 +1,23 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import style from './style';
 
-const Home = () => (
-	<div class={style.home}>
-		<h1>Home</h1>
-		<p>This is the Home component.</p>
-	</div>
-);
+class Home extends Component {
+
+	componentDidMount() {
+		fetch('https://hacker-news.firebaseio.com/v0/topstories.json').then((response) => response.json()).then((json) => {
+			// eslint-disable-next-line no-console
+			console.log(json);
+		});
+	}
+
+	render() {
+		return (
+			<div class={style.home}>
+				<h1>Home</h1>
+				<p>This is the Home component.</p>
+			</div>
+		);
+	}
+}
 
 export default Home;
